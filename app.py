13962f30609ff1856535b9f7ff7041b8654c1c11
Rowland <rowlandz@outlook.com>
2020-11-04 17:35:22 -0500

    update scatter plot graphics

--- app.py
+++ app.py
@@ -19,17 +19,38 @@ def get_data():
 
 df = get_data()
 
+# page layout
 st.title("Political Spectrum Voting Method Simulation")
-st.markdown("markdown text")
+st.markdown("First let's create a very simplified view rerpesenting the political preferences of a population")
 
-st.header("header")
-st.markdown("more mark down text")
-
+st.header("The Data")
+st.markdown("here we have a set of random x-y coordinate data")
 st.dataframe(df)
 st.markdown(df.shape)
 st.markdown(df.columns)
 
-fig = px.scatter(df, x="x_preference", y="y_preference")
+st.markdown("let's plot that on a graph to visualize it")
+fig = px.scatter(df, x="x_preference", y="y_preference", 
+    labels={
+        "x_preference": "Apples(-) or Oranges(+)", 
+        "y_preference": "Pizza: With(-) or Without(+) Pineapple"
+    }, 
+    title="Population Political Spectrum Distribution"
+)
+fig.update_traces(
+    marker=dict(
+        size=7,
+        line=dict(width=2, color="DarkSlateGrey")
+    ),
+    selector=dict(mode="markers")
+)
 st.plotly_chart(fig)
 
+st.markdown("Great! In this simplified society, there are only two important issues: ")
+st.markdown("weather or not pineapple belongs on pizza, shown by the y-axis, ")
+st.markdown("and which is better: apples or oragnes, shown by the x-axis")
+st.markdown("this map gives you an idea of how strongly our population feels about each subject, and where they fall on the political spectrum.")
+
+st.header("First Method: TODO i forgot what it was called???")
+st.markdown("the first blah blah blah")
 # https://towardsdatascience.com/streamlit-101-an-in-depth-introduction-fc8aad9492f2
